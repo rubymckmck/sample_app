775a1b02eb52703517cdc91e3ed14e9982230840
rubymckmck <cjcmckernan@me.com>
2013-03-27 23:20:48 +0000

    Finish Static Pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,13 +4,13 @@ describe "StaticPages" do
   describe "Home Page" do
   	it "should have the text 'Sample App'" do 
   		visit '../static_pages/home'
-  		page.should have_selector('h1', :text => 'Sample Application')
+  		page.should have_selector('h1', :text => 'Sample App')
   	end
 
-  	it "should have the title 'Sample App'" do 
-  		visit '../static_pages/home'
-  		page.should have_selector('title', :text => 'Sample Application')
-  	end
+    it "should have the title 'ROR - Home'" do 
+      visit '../static_pages/home'
+      page.should have_selector('title', :text => 'ROR - HOME')
+    end
 
   end
   describe "Help page" do
@@ -18,11 +18,24 @@ describe "StaticPages" do
   		visit '../static_pages/help'
   		page.should have_selector('h1', :text => 'Help Page')
   	end
+
+    it "should have the title 'ROR - HELP'" do 
+      visit '../static_pages/home'
+      page.should have_selector('title', :text => 'ROR - HOME')
+    end
+
+
   end
   describe "About page" do
   	it "should have the text 'About Us'" do 
   		visit '../static_pages/about'
   		page.should have_selector('h1', :text => 'About Us')
   	end
+
+    it "should have the title 'ROR - ABOUT US'" do 
+      visit '../static_pages/home'
+      page.should have_selector('title', :text => 'ROR - HOME')
+    end
   end
+
 end
